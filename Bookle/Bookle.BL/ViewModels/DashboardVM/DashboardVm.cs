8c6be6aca6242a7b9af440989039e5b7b2b0371c
RefigeM <rafigam-ab108@code.edu.az>
2025-02-23 23:55:01 +0400

    Almost completed

---
+++ Bookle/Bookle.BL/ViewModels/DashboardVM/DashboardVm.cs
@@ -0,0 +1,15 @@
+﻿using Bookle.Core.Entities;
+
+namespace Bookle.BL.ViewModels.DashboardVM;
+
+public class DashboardVm
+{
+	public List<Book> Books { get; set; }
+	public List<Author> Authors { get; set; }
+	public List<Blog> Blogs { get; set; }
+	public List<User> Users { get; set; }
+	public List<Book> TopRatedBooks { get; set; } = new();
+
+
+
+}
--- Bookle/Bookle.MVC/Areas/Admin/Controllers/DashboardController.cs
+++ Bookle/Bookle.MVC/Areas/Admin/Controllers/DashboardController.cs
@@ -1,16 +1,41 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Bookle.BL.Services.Interfaces;
+using Bookle.BL.ViewModels.DashboardVM;
+using Bookle.Core.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace Bookle.MVC.Areas.Admin.Controllers
 {
 	[Area("Admin")]
 	[Authorize(Roles ="Admin")]
 
-	public class DashboardController : Controller
+	public class DashboardController(IBookService _bookService,IAuthorService _authorService,IBlogService _blogService, IUserService _userService, UserManager<User> _userManager) : Controller
 	{
-		public IActionResult Index()
+		public async Task<IActionResult> Index()
 		{
-			return View();
+			var books = await _bookService.GetAllBooksWithDetailsAsync();
+			var authorsWithBookCounts = await _authorService.GetAuthorsWithBookCounts();
+			var blogs = await _blogService.GetAllPostsVisiblePostsAsync();
+			var authors = await _authorService.GetAllAuthorsWithBooksAsync();
+			var topRatedBooks = await _bookService.GetTopRatedBooksAsync(6);
+
+			var users = _userService.GetAllUsers();
+
+
+
+			var model = new DashboardVm
+			{
+				Books = books.ToList(),
+				Authors = authors.ToList(),
+				Blogs = blogs.ToList(),
+				TopRatedBooks = topRatedBooks.ToList(),
+				Users= users.ToList()
+			};
+
+			return View(model);
 		}
 	}
 }
